f6f1dd39a1d1008944bc865b46d343e3a7928b34
Sumanmhalsank02 <sumanmhalsank2000@gmail.com>
2023-04-11 20:21:28 -0400

    UPDATED: pages file

--- pages/posts/[id] 2.js
+++ pages/posts/[id] 2.js
@@ -21,7 +21,7 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
     paths,
