62e7d70798ac30fe57aa5f1980e166312671271c
Srinivas <123528673+srinivasbojja369@users.noreply.github.com>
2023-05-08 10:16:49 -0400

    UPDATED: Blog Page

--- pages/Blogs.js
+++ pages/Blogs.js
@@ -15,6 +15,9 @@ export default function Blogs() {
         <p>Here at MyWebClass.org, we offer a wide range of courses in web development, DevOps, and management. Our expert instructors are passionate about their fields and are committed to helping you achieve your goals.</p>
 
         <h2>Web Development Courses</h2>
+        <div style={{}}>
+            <img style={{ height: '460px', width: '460px'}} src="images/web dev.png" alt=" web development" width="720px" />
+       </div>
         <p>Our web development courses cover a variety of topics, from HTML and CSS to JavaScript frameworks like React and Angular. Whether you're just starting out or looking to expand your skills, we have a course that's right for you.</p>
 
         <h3>Some of Our Most Popular Web Development Courses:</h3>
@@ -26,6 +29,9 @@ export default function Blogs() {
         </ul>
 
         <h2>DevOps Courses</h2>
+        <div style={{}}>
+            <img style={{ height: '460px', width: '460px'}} src="images/devops.png" alt=" web development" width="720px" />
+       </div>
         <p>Our DevOps courses are designed to help you master the tools and processes that are essential for building and deploying modern applications. From continuous integration and deployment to containerization and orchestration, we've got you covered.</p>
 
         <h3>Some of Our Most Popular DevOps Courses:</h3>
@@ -37,6 +43,9 @@ export default function Blogs() {
         </ul>
 
         <h2>Management Courses</h2>
+        <div style={{}}>
+            <img style={{ height: '460px', width: '460px'}} src="images/management.png" alt=" web development" width="720px" />
+       </div>
         <p>Our management courses are perfect for anyone looking to develop their leadership skills and advance their careers. From project management and team leadership to strategic planning and business analysis, we offer a wide range of courses to help you achieve your goals.</p>
 
         <h3>Some of Our Most Popular Management Courses:</h3>
