b32c509c4bb2a3a55155babdd1a80bddd3d45816
Suman Mhalsank <48148023+Sumanmhalsank02@users.noreply.github.com>
2023-05-07 22:16:18 -0400

    CHANGED: test page

--- tests/page_titles.spec.js
+++ tests/page_titles.spec.js
@@ -1,16 +1,26 @@
 const { test, expect } = require('@playwright/test');
 const { chromium } = require('playwright');
+const path = require('path');
+
+const config = require(path.join(process.cwd(), 'playwright.config.js'));
+const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'));
 
 const TIMEOUT = 30000;
-
-test('Website should have title "MyWebclass.org"', async () => {
+async function checkPageTitle(pageUrl, expectedTitle) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-
-  await page.goto('https://njit-wis.github.io/project-2-team-ssh/', { timeout: TIMEOUT });
+  await page.goto(pageUrl, { timeout: TIMEOUT });
   const pageTitle = await page.title();
-
   await browser.close();
+  expect(pageTitle).toBe(expectedTitle);
+}
 
-  expect(pageTitle).toBe('MyWebclass.org');
+pages.forEach((page) => {
+
+  test(`Page "${page.path}" should have the correct title`, async ({}) => {
+    const pageUrl = `${config.use.baseURL}${page.path}`;
+
+    const expectedTitle = page.title;
+    await checkPageTitle(pageUrl, expectedTitle);
+  });
 });
