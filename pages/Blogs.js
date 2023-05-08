e41324dd687f88bc7c084b171c1e9a5dbf10c9c6
Sumanmhalsank02 <sumanmhalsank2000@gmail.com>
2023-05-08 11:08:30 -0400

    ADDED: A signup popup for weekly blogs

--- pages/Blogs.js
+++ pages/Blogs.js
@@ -9,6 +9,14 @@ export default function Blogs() {
     <Layout home>
       <Head>
         <title>MyWebClass.org | Blogs</title>
+        <script id="mcjs" dangerouslySetInnerHTML={{
+          __html: `
+            !function(c,h,i,m,p){
+              m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/d7e6ee271d33e9808fc4971ea/087ba75587caa60bd84d010f2.js");
+          `
+        }} />
+
       </Head>
       <div>
         <h1>Welcome to Our Blog!</h1>
