8ca53488bd2b8016461b080b44f4b05be7d79c24
Sumanmhalsank02 <sumanmhalsank2000@gmail.com>
2023-05-09 20:02:56 -0400

    UPDATES

--- pages/About.js
+++ pages/About.js
@@ -11,26 +11,25 @@ const About = () => {
             <title>MyWebClass.org | About</title>
         </Head>
         <section className={utilStyles.headingMd}>
-     <div style={{marginLeft: '600px', justifyContent: 'space-between'}}>
-  <h1 className="about-header">Team Members</h1>
-  <div className="team-members">
-    {[    { name: 'Keith Williams', designation: 'Professor', photo: '/john-doe.jpg' },
-      { name: 'Suman S. Mhalsank', designation: 'Developer', photo: '/john-doe.jpg' },
-      { name: 'Srinivas Bojja', designation: 'Project Manager', photo: '/jane-smith.jpg' },
-        { name: 'Harshith Reddy', designation: 'Tester', photo: '/bob-johnson.jpg' },   ].map((member) => (
-      <div key={member.name} className="team-member">
-        <img src={member.photo} alt={member.name} />
-        <div className="member-info">
-          <h2>{member.name}</h2>
-          <p>{member.designation}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-  </div>
-</section>
-
-      </Layout>
+          <h1 className="about-header">Team Members</h1>
+          <div className="team-members" style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {[
+              { name: 'Keith Williams', designation: 'Professor', photo: 'images/Keith.png'},
+              { name: 'Suman S. Mhalsank', designation: 'Developer', photo: '/john-doe.jpg' },
+              { name: 'Srinivas Bojja', designation: 'Project Manager', photo: '/jane-smith.jpg' },
+              { name: 'Harshith Reddy', designation: 'Tester', photo: '/bob-johnson.jpg' },
+            ].map((member) => (
+              <div key={member.name} className="team-member" style={{ flex: '1', margin: '0 10px' }}>
+                <img src={member.photo} alt={member.name} style={{ borderRadius: '50%', width: '200px', height: '200px' }} />
+                <div className="member-info" style={{ textAlign: 'center' }}>
+                  <h2>{member.name}</h2>
+                  <p>{member.designation}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+   </Layout>
   );
 };
 
