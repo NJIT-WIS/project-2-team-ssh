31265f67b93f5a77304c089b77a7ed897fe8d743
Suman Mhalsank <48148023+Sumanmhalsank02@users.noreply.github.com>
2023-05-12 12:59:28 -0400

    Update Blogs.js

--- pages/Blogs.js
+++ pages/Blogs.js
@@ -2,7 +2,10 @@ import React from 'react'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
-
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import styles from '../styles/contact.module.css'
+import Date from '../components/date'
 
 export default function Blogs({ allPostsData }) {
   return (
